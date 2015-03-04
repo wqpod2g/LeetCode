4a83003f9ff4dccee1550e8f8b1485fdb466724f
wang qiang <wqpod2g@gmail.com>
2015-03-04 09:24:08 +0800

    Longest Palindromic Substring

--- src/Solution/LeetCode5.java
+++ src/Solution/LeetCode5.java
@@ -1,31 +1,33 @@
 package Solution;
 
+/**
+ * Given a string S, find the longest palindromic substring in S. 
+ * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ * @author wangqiang
+ *
+ */
 public class LeetCode5 {
 	
-	 public static boolean isPalindrome(String str) {
-	        int size = str.length();
-	        for(int i=0;i<=(size-1)/2;i++) {
-	            if(str.charAt(i)!=str.charAt(size-1-i)) return false;
-	        }
-	        return true;
-	    }
-	    
-	    public static String longestPalindrome(String s) {
-	        int size = s.length();
-	        int longest = 0;
-	        String re = "";
-	        for(int i=0;i<size;i++) {
-	            for(int j=i+1;j<size;j++) {
-	                int length = j-i+1;
-	                String substring = s.substring(i,j+1);
-	                if(isPalindrome(substring)&&length>longest) {
-	                    re = substring;
-	                    longest = length;
-	                }
-	            }
-	        }
-	        return re;
-	    }
+	public static String subPalindrome(String s,int i,int j) {
+        while(i>=0&&j<=s.length()-1&&s.charAt(i)==s.charAt(j)) {
+            i--;
+            j++;
+        }
+        return s.substring(i+1,j);
+    }
+    
+    public static String longestPalindrome(String s) {
+        int size = s.length();
+        String re = "";
+        for(int i=0;i<size;i++) {
+            String substring = subPalindrome(s,i,i+1);
+            if(substring.length()>re.length()) re = substring;
+            substring = subPalindrome(s,i-1,i+1);
+            if(substring.length()>re.length()) re = substring;
+        }
+        
+        return re;
+    }
 	    
 	    public static void main(String[] args) {
 	    	System.out.println(longestPalindrome("nmngaowrbsssvihklwmuqshcddwlxrywrlwtennwfvrevgvhsvgeccfulmuvrcksdmgeqrblnlwoepefhcwhmgyvgcoyyygrmttyfycxwbqktpurlcfhzlakhmrddsydgygganpmaglaxyhfwjusukzcnakznygqplngnkhcowavxoiwrfycxwdkxqfcjqwyqutcpyedbnuogedwobsktgioqdczxhikjrbkmqspnxcpngfdwdaboscqbkwforihzqdcppxjksiujfvlpdjryewaxgmdgigvxdlstxwngtbdrrkfudjinzyxbdmkautclvvyguekuzwwetmsxittgtxbnvvrgasvnlogdiepltweaehubwelznidltzlbzdsrxmhjpkmylnwkdsxnpkplkdzywioluaqguowtbaoqzqgjfewphqcvlnwlojbxgomvxxkhwwykawegxubjiobizicuxzeafgautefsurgjlbhcfevqzsbhwxycrcaibdsgluczcuewzqupakbzmcvzsfodbmgtugnihyhqkvyeboqhqldifbxuaxqzxtyejoswikbzpsvzkxcndgeyvfnyrfbkhlalzpqjueibnodamgpnxlkvwvliouvejcpnakllfxepldfmdzszagkyhdgqqbkb"));
